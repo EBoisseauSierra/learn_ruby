afd75ba82e9896d827ee15175a8f4fce27e251ae
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-06-29 12:36:39 -0400

    Add solution to 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,24 @@
-#write your code here
+def echo argument
+    argument
+end
+
+def shout argument
+    argument.upcase
+end
+
+def repeat(argument, number_of_times = 2)
+    argument + " #{argument}" * (number_of_times - 1)
+end
+
+def start_of_word(word, length)
+    word[0...length]
+end
+
+def first_word sentence
+    sentence.split[0]
+end
+
+def titleize title
+    little_words = ['and', 'the', 'over']
+    title.split.each_with_index.map { |e, i| ((i != 0) && (little_words.include? e)) ? e : e.capitalize }.join(' ')
+end
