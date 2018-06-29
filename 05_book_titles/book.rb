168d121cd5362fdf4facd62177aaa6aa1db4a783
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-06-29 13:11:47 -0400

    Add solution to 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,8 @@
 class Book
-# write your code here
+    attr_accessor :title
+
+    def title=(input_title)
+        words_to_not_capitalize = %w(the a an and in the of)
+        @title = input_title.split.each_with_index.map { |e, i| (i != 0 && (words_to_not_capitalize.include? e)) ? e : e.capitalize }.join(' ')
+    end
 end
