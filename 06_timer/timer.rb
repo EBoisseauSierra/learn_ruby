e148197d0f14a8be1d674702c407344c74bd044c
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-06-29 13:30:48 -0400

    Add solution to 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,22 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds, :minutes, :hours
+  def initialize
+      @seconds = 0
+      @minutes = 0
+      @hours = 0
+  end
+
+  def seconds=(input_seconds)
+      self.minutes = input_seconds / 60
+      @seconds = input_seconds % 60
+  end
+
+  def minutes=(input_minutes)
+      self.hours = input_minutes / 60
+      @minutes = input_minutes % 60
+  end
+
+  def time_string
+      "#{"%02d" % @hours}:#{"%02d" % @minutes}:#{"%02d" % @seconds}"
+  end
 end
