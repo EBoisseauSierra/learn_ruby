610355b0560073ab717c6cf751557c8ff5df03a9
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-06-29 12:15:30 -0400

    Add results to 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,29 @@
-#write your code here
+def add(a,b)
+    a + b
+end
+
+def subtract(a,b)
+    a - b
+end
+
+## self dirty solution
+# def sum array
+#     result = 0
+#     array.each{| element | result += element}
+#     result
+# end
+def sum array
+    array.inject(0) {|result, element| result + element}
+end
+
+def multiply(*arguments)
+    arguments.inject(1) {|result, element| result * element}
+end
+
+def power(base, exponent)
+    base ** exponent
+end
+
+def factorial number
+    number == 0 ? 1 : number * factorial(number - 1)
+end
